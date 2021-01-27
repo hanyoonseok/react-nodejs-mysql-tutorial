3fb31d7a05086b768e4789c966dc46516a3fe20d
윤석 한 <hys6078@naver.com>
2021-01-27 20:21:12 +0900

    react-nodejs-mysql 연결하려면 이거 참고해

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react';
+import './style.css';
+import Axios from 'axios';
 
 function App() {
+  const [movieName, setMovieName] = useState('');
+  const [review, setReview] = useState('');
+  const [movieReviewList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    Axios.get('http://localhost:3001/api/get')
+    .then((response)=>{
+      setMovieList(response.data)
+      })
+  },[])
+  const submitReview =()=>{
+    Axios.post('http://localhost:3001/api/insert', 
+    {movieName: movieName, movieReview:review})
+    .then(()=>{
+      alert("success");
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>hello</div>
+      <input type="text" name="name" onChange={(e)=>{
+        setMovieName(e.target.value)
+      }}/>
+      <input type="text" name="name" onChange={(e)=>{
+        setReview(e.target.value)
+      }}/>
+      <button onClick={submitReview}>submit</button>
+      {movieReviewList.map((val)=>{
+        return <h1>MovieName:{val.movieName} | Movie Review: {val.movieReview}</h1>
+      })}
     </div>
   );
 }
